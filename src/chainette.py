49d2e5e533d183d20535e1ce2efa5142a63cea12
Brateau Quentin <quentin.brateau@ensta-bretagne.org>
2021-03-03 16:30:54 +0100

    Fixing plot problem

--- src/chainette.py
+++ src/chainette.py
@@ -34,13 +34,14 @@ if __name__ == "__main__":
     t = np.linspace(x1, x2, 100)
     c = a*np.cosh((t+c1)/a)+c2
 
+    plt.plot(t, c)
     plt.scatter(x1, y1)
     plt.scatter(x2, y2)
-    plt.plot(t, c)
 
     # Initializing each node
     for i in range (1, n-1):
         x, y =  fsolve(g, (x1+i*(x2-x1)/(n-1), a*np.cosh((x1+i*(x2-x1)+c1)/((n-1)*a))+c2), args=(i))
+        print(i)
         plt.scatter(x, y, color="crimson")
 
     plt.axis("equal")
