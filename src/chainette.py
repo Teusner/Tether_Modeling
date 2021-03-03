329042a13c36c571dfbe919b3910115b669d1214
Brateau Quentin <quentin.brateau@ensta-bretagne.org>
2021-03-03 15:48:32 +0100

    Updating initialization

--- src/chainette.py
+++ src/chainette.py
@@ -2,10 +2,6 @@ import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 
-L = 10
-x1, y1 = 1, 0
-x2, y2 = 5, 6
-n = 10
 
 def f(x):
     global L, x1, x2, y1, y2
@@ -15,10 +11,6 @@ def f(x):
     eq3 = a*np.cosh((x2+c1)/a) + c2 - y2
     return [eq1, eq2, eq3]
 
-
-
-a, c1, c2 =  fsolve(f, (0.8, (x1+x2)/2, (y1+y2)/2), factor=0.1)
-
 def g(p, i):
     global L, a, c1, c2, n
     global x1
@@ -26,20 +18,31 @@ def g(p, i):
     eq2 = a*np.cosh((p[0]+c1)/a) + c2 - p[1]
     return [eq1, eq2]
 
-t = np.linspace(x1, x2, 100)
-c = a*np.cosh((t+c1)/a)+c2
 
-print(a, c1, c2)
-print(a*np.cosh((x1+c1)/a)+c2)
-print(a*np.cosh((x2+c1)/a)+c2)
-print(a*np.sinh((x2+c1)/a) - a*np.sinh((x1+c1)/a))
+if __name__ == "__main__":
+    # Catenary parameters
+    L = 10
+    x1, y1 = 0, 0
+    x2, y2 = 6, 3
+    n = 10
 
-for i in range (1, n-1):
-    x, y =  fsolve(g, (x1+i*(x2-x1)/(n-1), a*np.cosh((x1+i*(x2-x1)+c1)/((n-1)*a))+c2), args=(i))
-    plt.scatter(x, y, color="crimson")
+    # Getting catenary parameters
+    a, c1, c2 =  fsolve(f, (0.8, (x1+x2)/2, (y1+y2)/2), factor=0.1)
+    print("a: {}, c1: {}, c2: {}".format(a, c1, c2))
 
-plt.scatter(x1, y1)
-plt.scatter(x2, y2)
-plt.plot(t, c)
-plt.grid(True)
-plt.show()
+    # Plotting the catenary curve
+    t = np.linspace(x1, x2, 100)
+    c = a*np.cosh((t+c1)/a)+c2
+
+    plt.scatter(x1, y1)
+    plt.scatter(x2, y2)
+    plt.plot(t, c)
+
+    # Initializing each node
+    for i in range (1, n-1):
+        x, y =  fsolve(g, (x1+i*(x2-x1)/(n-1), a*np.cosh((x1+i*(x2-x1)+c1)/((n-1)*a))+c2), args=(i))
+        plt.scatter(x, y, color="crimson")
+
+    plt.axis("equal")
+    plt.grid(True)
+    plt.show()
